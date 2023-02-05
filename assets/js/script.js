78b8127cfdaed34d15938cdd24d15fbc84bd7461
lucasrenandns <lucasrenandns96@gmail.com>
2023-02-05 20:09:53 -0300

    Updated Website

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ const header = document.getElementById("header")
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", this.window.scrollY > 0)
 })
+
+const menuIcon = document.getElementById("menu-icon")
+const navBar = document.querySelector(".navbar")
+
+menuIcon.addEventListener("click", function() {
+    navBar.classList.toggle("open")
+    menuIcon.classList.toggle("bx-x")
+})
+
+window.addEventListener("scroll", function() {
+    navBar.classList.remove("open")
+    menuIcon.classList.remove("bx-x")
+})
